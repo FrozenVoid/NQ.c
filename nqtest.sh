b58235a41f7812e0b2edd14dc45078549ecda84b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-16 08:01:19 +0200

    Update nqtest.sh

--- nqtest.sh
+++ nqtest.sh
@@ -1,6 +1,6 @@
 gcc nq.c -Ofast -march=native -mtune=native -Wall -pipe -onq
-for (( n=8;n<100000;n++ ))
+for (( n=8;n<100000000;n++ ))
 do
   echo "Test N-Queens= $n  "
-   time ./nq $n
+   ./nq $n
 done
