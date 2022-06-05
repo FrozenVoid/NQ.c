9a41e42859c4353f4569be9c9209f4304368a5a1
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 10:49:08 +0300

    Update dualswap.h

--- Functions/dualswap.h
+++ Functions/dualswap.h
@@ -1,6 +1,7 @@
 //dual swap algo
 while(cur>2){
-A=fstgcols(B);B=fstgcols(A);
+A=fstgcols(A);
+do{B=fstgcols(B);}while(A==B);
 info();//new iteration update
 dir=1;swapc(A,B);cur=countudiag();
 if(cur>best){dir=-1;fail++;
