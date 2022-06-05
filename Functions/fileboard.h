7d81fbd01b6a48672127d78d5f86fe60a7b35940
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 10:04:51 +0300

    Create fileboard.h

---
+++ Functions/fileboard.h
@@ -0,0 +1,9 @@
+void fileboard(){char* fname=malloc(126);
+sprintf(fname,"%"PRIu64".nq",(u64)N);
+FILE*out=fopen(fname,"wb");
+print("\nWriting board into",fname);
+size_t rfwout=fwrite(board,4,N,out);
+if(rfwout==N){print("\nFile:",fname,"\nRecorded board as file\n");}
+else{print("\nFile:",fname,"\nWrite mismatch\n",N,"!=",rfwout);}
+fclose(out);
+}
