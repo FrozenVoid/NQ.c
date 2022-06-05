722fc66f373f9cf4882ad7b0571f529447e5b251
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 00:09:42 +0300

    Update zerocols.h

--- Functions/zerocols.h
+++ Functions/zerocols.h
@@ -1,9 +1,5 @@
 static inline int zerocols(val_t P){//1= no collision,>1 collisions
 const val_t s=board[P];
-FETC(&diagL[s+P],0,0);
 if((diagL[s+P])!=1)return 0;
-FETC(&diagR[s+(N-P)],0,0);
 if(diagR[s+(N-P)]!=1)return 0;
-return 1;
-
-}
+return 1;}
