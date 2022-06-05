2775bff8db8d9d0a66b56d178154b8bcfa3394c4
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 11:13:56 +0300

    Update endsearch.h

--- Functions/endsearch.h
+++ Functions/endsearch.h
@@ -3,22 +3,18 @@ print("\n\n\n\n\n\n\n\n\nStage2:",cur,"cols Time:",mstime(),"ms\n");
 #include "dualswap.h"
 print("\n\n\n\nSTage3:",cur,"cols Time:",mstime(),"ms\n");
 endsearch:;
-A=rndcell();
-if(!(randuint64()&127))A=fstgcols(B);
-B=fstgcols(A);
-while(randuint64()&3)B=fstgcols(B);
-if(A==B)goto endsearch;
+A=fstgcols(A);;//switch randomly
+end2:;
+uint64_t rsw=randuint64();
+
+if(!(rsw&7))A=rndcell();
+B=rndcell();
+if(A==B)goto end2;
 info();//new iteration update
 dir=1;swapc(A,B);cur=countudiag();
 if(cur>best){dir=-1;fail++;
-swapc(A,B);goto endsearch;}
+swapc(A,B);goto end2;}
 tfail+=fail;swapt+=swaps;
 fail=0;swaps=0;best=cur;//new record
 if(cur==0)goto fin;
-/*if(cur<4){val_t mA=fstgcols(A);
-print("\nFInal array:",cur,"\n");
-print(mA);while(A!=mA){print(A);A=fstgcols(A);}
-
-goto fin;
-}*/
 goto endsearch;
