0d31b8a3d899b2e5c6493fd778c96504b05e1cfa
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 09:55:18 +0300

    Update nq.c

--- nq.c
+++ nq.c
@@ -160,20 +160,9 @@ if(!(i&0xfff)){putchar('+');fflush(stdout);}
   size_t zqueen=board[z];
  if(((z-i)==(zqueen-cur))||((z-i)==(cur-zqueen))){return i; };   }  }
 return 0;}
-//======prefill counts and start solver===
-void solve(void){
-//fill queen location counts
-for(size_t i=0;i<N;i++){diagL[board[i]+i]++;}
-for(size_t i=0;i<N;i++){diagR[board[i]+(N-i)]++;}
-//calculate Sums of collisions
-
-for(size_t i=0;i<N*2;i++){sumL+=(diagL[i]-1)*(diagL[i]>1);}
-for(size_t i=0;i<N*2;i++){sumR+=(diagR[i]-1)*(diagR[i]>1);}
-linearsolve();
 
 
-
-}
+#include "Functions/solve.h"
 #include "Functions/integrity.h"
 #include "Functions/presolve.h"
 #include "Functions/scramble.h"
