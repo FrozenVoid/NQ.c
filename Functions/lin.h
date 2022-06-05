c6ca88fc66f05db46ce062999645b07848c88f11
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 13:36:02 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -2,10 +2,10 @@
 void linearsolve(){
  A=0,B=0;cend=__rdtsc();
  size_t NL=log2index(N);
- u64 lc=0,lcmax=(N)/NL,minstage2=((NL)*(NL)),endsearch=(NL/2)*(NL/2);
+ u64 lc=0,lcmax=(N)/NL,minstage2=N<10000?1000000:((NL)*(NL)),endsearch=(NL/2)*(NL/2);
 
- cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime());goto endl;/*presolved*/}
-print("\nT:",mstime()," ms Collisions:",cur);fflush(stdout);
+ cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"\n");goto endl;/*presolved*/}
+print("\nT:",mstime()," ms Collisions:",cur,"\n");fflush(stdout);
 //--------Main loop-------------
 #include "windowslide.h"
 if(cur!=0){info();goto first;}
