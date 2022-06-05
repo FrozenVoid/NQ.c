1c91f3a4ee6b23163609d6817de6de6b1ce6b902
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 00:09:12 +0300

    Update rndcell.h

--- Functions/rndcell.h
+++ Functions/rndcell.h
@@ -1,13 +1,17 @@
-
 static inline val_t rndcell(){
  val_t r=modreduce((val_t)randuint64(),N);
- FETC(&board[r],0,2);
+FETC(&board[r],0,2);
+FETC(&diagL[board[r]+r],0,2);
+FETC(&diagR[board[r]+(N-r)],0,2);
  return r;
  }
 
 static inline val_t rndcell2(val_t start,val_t rangemax){
  val_t r=start+modreduce((val_t)randuint64(),rangemax);
- FETC(&board[r],0,2);
+//prefetch earlier for diagonal data
+FETC(&board[r],0,2);
+FETC(&diagL[board[r]+r],0,2);
+FETC(&diagR[board[r]+(N-r)],0,2);
 
  return r;
  }
