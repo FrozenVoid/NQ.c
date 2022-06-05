2550701eba7d4f53b3f81d4d4dea7e698da4a87b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 15:12:39 +0300

    Update verifier.h

--- Functions/verifier.h
+++ Functions/verifier.h
@@ -4,10 +4,10 @@ for(size_t i=0;i<N;i++){
 //halt on error(stops nqtest.sh)
 val_t left=(diagL[board[i]+i]);
 val_t right=(diagR[board[i]+(N-i)]);;
-if(left!=1||right!=1){
+if(left==1 && right==1)continue;
+
 print("Invalid solution to N=",N,"Collision at c,L,R:",i,left,right,"\nPress enter to see board/CTRL-C break\n");
-fflush(stdout);
 char __attribute__((unused))  tt=getchar();
 printboard(',');
-return;}
+return;
 }}
