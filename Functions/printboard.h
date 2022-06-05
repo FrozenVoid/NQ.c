c70f1442c2408308a03428890a9f490caee5345c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 18:23:38 +0300

    Update printboard.h

--- Functions/printboard.h
+++ Functions/printboard.h
@@ -1,5 +1,4 @@
-//--------------------------
-void printboard(char sep){print("\n");
- print(board[0]);
+void printboard(char sep){
+ printf("\n%"PRIu64"",(uint64_t)board[0]);
 for(size_t i=1;i<N;i++){
-print(sep,board[i]);}}
+printf("%c%"PRIu64"",sep,(uint64_t)board[i]);}}
