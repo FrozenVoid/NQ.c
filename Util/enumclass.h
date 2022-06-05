388b3345af7396f8cb63c47a04385012690aebd5
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 18:08:56 +0300

    Delete Util directory

--- Util/enumclass.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/* enumc(name,args...) 
-create a strongly typed enum using unions.
-
-*/
-#define enumc(name,args...) typedef union name{}name;name args;
-#define isenumeq(a,b) ((&a)==(&b))
--- Util/fileio.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "arguments.h"
-#include <stdio.h>
-#define filesize(fptr) ({ atype curpos=ftell(fptr);\
-fseek(fptr,0,SEEK_END);atype retsize=ftell(fptr);\
-fseek(fptr,curpos,SEEK_SET);retsize;})
