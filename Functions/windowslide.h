bc190b103addb717339ea04cb8527bd8ded9f4fd
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 16:19:39 +0300

    Update windowslide.h

--- Functions/windowslide.h
+++ Functions/windowslide.h
@@ -12,7 +12,7 @@ midloop:;
 info("Midloop:");//midloop test
 #endif
 if(A==B){fail++;
-if(fail>minstage2)goto endsearch3;
+if(fail>minstage2)return esearch();
 goto second;}
 dir=1;swapc(A,B);cur=countudiag();
 fail+=cur>=best;
@@ -21,15 +21,12 @@ swapc(A,B);
 #ifdef VERBOSE
 info("Fail:");//fail test
 #endif
-if(fail>minstage2)goto endsearch3;
+if(fail>minstage2)return esearch();
 goto second;}
 tfail+=fail;swapt+=swaps;
 info(cur==best?"Swap0:":"Swap+:");//new iteration update
 fail=cur>=best?fail:0;swaps=0;best=cur;//new record
 if(cur==0){return;}
 if(fail<minstage2)goto innerc;
-#include "endsearch.h"
-
-}
-if(cur!=0){info("Loop next:");goto first;}
-}
+return esearch();
+}}
