43b38e76909bb549f4d5f5057edf263fb7f9cc1c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 00:10:18 +0300

    Update qcccount.h

--- Functions/qcccount.h
+++ Functions/qcccount.h
@@ -2,6 +2,4 @@
 static inline val_t qccount(val_t P){
 //cannot be zero due being set from q[]
 val_t s=board[P];
-FETC(&diagL[s+P],0,0);
-FETC(&diagR[s+(N-P)],0,0);
 return ((diagL[s+P]))+((diagR[s+(N-P)]))-2;}
