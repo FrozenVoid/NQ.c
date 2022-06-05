3ca48714ebca05c95c29adbed916c39fce1d6ec5
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 14:56:39 +0300

    Update windowslide.h

--- Functions/windowslide.h
+++ Functions/windowslide.h
@@ -3,14 +3,16 @@ for(size_t i=0;i<N ;i++){innerc:;
 if(zerocols(i))continue;
 A=i;
 second:;lc=0;
-do{
-B=i+1+modreduce((val_t)randuint64(),N-i-1);}while(lc++<lcmax &&zerocols(B));
+do{lc++;
+B=i+1+modreduce((val_t)randuint64(),N-i-1);}while(lc<lcmax &&zerocols(B));
 midloop:;
 if(A==B)goto second;
 dir=1;swapc(A,B);cur=countudiag();
 fail+=cur>=best;
-if(cur>best){dir=-1;
-swapc(A,B);goto second;}
+if(cur>best){dir=-1;//info();//new iteration update
+swapc(A,B);
+if(fail>minstage2)goto endsearch3;
+goto second;}
 tfail+=fail;swapt+=swaps;
 info();//new iteration update
 fail=cur>=best?fail:0;swaps=0;best=cur;//new record
