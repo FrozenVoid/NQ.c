23774a2caba29fe6c5451fd0a1d1988db30c8a78
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 09:57:15 +0300

    Update nq.c

--- nq.c
+++ nq.c
@@ -153,14 +153,8 @@ print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
 endl:; //end loop
 fflush(stdout);}
 
-size_t diags(val_t*board,size_t len){//first collision
-for(size_t i=0;i<len;i++){size_t cur=board[i];
-if(!(i&0xfff)){putchar('+');fflush(stdout);}
- for(size_t z=i+1;z<len;z++){
-  size_t zqueen=board[z];
- if(((z-i)==(zqueen-cur))||((z-i)==(cur-zqueen))){return i; };   }  }
-return 0;}
 
+#include "Functions/diags.h"
 
 #include "Functions/solve.h"
 #include "Functions/integrity.h"
