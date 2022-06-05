8397c6f28c09f9b5fa5483c8b5b49ff38a9504e9
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 12:01:11 +0300

    Create syntax.h

---
+++ Functions/syntax.h
@@ -0,0 +1,3 @@
+void syntax(){puts("Syntax:nq N [p|f|t|c|i] [filename|sep]\n N=Board size min=8 \n p [string]=printboard [separator] \n f=write result as file \nt=test presolved array\n i filename=load u32/u64 queen array filename\n num+s =scramble rows num times(N*num)\nc= additional checks for integrity(slow)");exit(1);
+
+}
