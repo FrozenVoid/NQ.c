bdf18630428f2c9c4772314d4a8b1e01e929e95b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 10:23:17 +0300

    Create lin.h

---
+++ Functions/lin.h
@@ -0,0 +1,16 @@
+//--------mainloop------
+void linearsolve(){
+ A=0,B=0;cend=__rdtsc();
+ size_t NL=log2index(N);
+ u64 lc=0,lcmax=(N)/NL,minstage2=((NL+8)*(NL+8)),endsearch=(NL/2)*(NL/2);
+
+ cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime());goto endl;/*presolved*/}
+print("\nT:",mstime()," ms Collisions:",cur);fflush(stdout);
+//--------Main loop-------------
+#include "windowslide.h"
+if(cur!=0){info();goto first;}
+
+fin:;
+print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
+endl:; //end loop
+fflush(stdout);}
