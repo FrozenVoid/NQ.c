2e1fd204e1151f15d9827b9d65395e5a360914fc
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 18:32:19 +0300

    Update printboard.h

--- Functions/printboard.h
+++ Functions/printboard.h
@@ -1,4 +1,4 @@
 void printboard(char sep){
  printf("\n%"PRIu64"",(uint64_t)board[0]);
-for(size_t i=1;i<N;i++){
-printf("%c%"PRIu64"",sep,(uint64_t)board[i]);}}
+for(size_t i=1;i<N;i++){putchar(sep);
+printf("%"PRIu64"",(uint64_t)board[i]);}}
