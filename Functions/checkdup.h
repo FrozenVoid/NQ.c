f1671918033d7f64b0a19b2002b2755fbd2c9cd7
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 15:14:31 +0300

    Update checkdup.h

--- Functions/checkdup.h
+++ Functions/checkdup.h
@@ -2,7 +2,7 @@ void checkdup(){
 for(size_t i=0;i<N;i++){
 const val_t c=board[i];
 for(size_t z=i+1;z<N;z++){
-if(board[z]==c){print("Duplicate column data (",c,")at:",z,i,"\n");
+if(board[z]==c){print("\nDuplicate column data (",c,")at:",z,i,"\n");
 print("Press enter to continue/CTRL-C end\n");
 char __attribute__((unused))  tt=getchar();;}}
 }}
