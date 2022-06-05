1836519a3863f794e881e21c04318ebfc29c4ce1
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 15:51:31 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -8,7 +8,7 @@ void linearsolve(){
 print("\nSTART:",mstime()," ms Collisions:",cur,"\n");fflush(stdout);
 //--------Main loop-------------
 #include "windowslide.h"
-if(cur!=0){info();goto first;}
+if(cur!=0){info("Loop next:");goto first;}
 
 fin:;
 print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
