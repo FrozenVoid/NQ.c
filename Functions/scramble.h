38e001f81f1789d3d235f01db6faa06d5a39afec
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 16:43:35 +0300

    Update scramble.h

--- Functions/scramble.h
+++ Functions/scramble.h
@@ -2,5 +2,6 @@ void scramble(size_t num){
  print("\nScrambling N=",N," Times:",num,"\n");fflush(stdout);
 
 for(size_t z=0;z<num;z++){
-for(size_t i=0;i<N;i++){swapq(board[i],board[rndcell()]);}
+for(size_t i=0;i<N;i++){
+swapq(i,rndcell());}
 }}
