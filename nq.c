bc8a3b48bc8128bd3a67c1f72d1c62dd03374f2f
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 09:59:41 +0300

    Update nq.c

--- nq.c
+++ nq.c
@@ -126,16 +126,7 @@ return ((sval_t) x * (sval_t) N) >> (sizeof(val_t)*8);}
  }
 //----linear collission count----------
 #define countudiag() (sumL+sumR)
-//------------------------
-#ifdef SILENCE
-#define info()
-#else
- void info(){
-  if(__rdtsc()-cend>NCYCLES  ){
-  clock_t Ntime=mstime();
-  print("\n cols=",best,"A=",A,"B=",B,"valid/fail:",swaps,"/",(fail),"\nswap:",1.0*swapt/Ntime,"fail:",1.0*tfail/Ntime);
-  print("\nT:",Ntime,"ms Col%",100.0*(cur)/N,"Swapt",swapt,"Valid%",100.0*swapt/tswaps);cend=__rdtsc();fflush(stdout);}}
-#endif
+#include "Functions/info.h"
 //--------mainloop------
 void linearsolve(){
  A=0,B=0;cend=__rdtsc();
