5130c230522a6951de4d0645e80ebc4579ec5a1b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 12:20:19 +0300

    Update printboard.h

--- Functions/printboard.h
+++ Functions/printboard.h
@@ -1,2 +1,5 @@
 //--------------------------
-void printboard(char* sep){print("\n");for(size_t i=0;i<N-1;i++)print(board[i]+1,sep);print(board[N-1],"\n");}
+void printboard(char sep){print("\n");
+ print(board[0]);
+for(size_t i=0;i<N;i++){
+print(sep,board[i]);}}
