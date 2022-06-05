d17b3ac79d27a821dcfae5c0c56428274ea84051
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-05 15:53:59 +0300

    Update nqtest.sh

--- nqtest.sh
+++ nqtest.sh
@@ -1,4 +1,4 @@
-gcc nq.c -Ofast -march=native -mtune=native -pipe -onq
+gcc nq.c -gdwarf -Ofast -march=native -mtune=native -pipe -onq -DVERBOSE
 for (( n=4;n<11111111;n++ ))
 do
   echo "Test N-Queens= $n  "
