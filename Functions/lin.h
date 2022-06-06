70e729e6e164cf3865c99f05384ec39f417666fa
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 06:12:53 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -3,13 +3,45 @@
 void linearsolve(){
  cend=__rdtsc();
  NL=log2index(N);
-  lc=0,lcmax=2+((N)/(NL*NL)),minstage2=160+((NL)),endsearch=(NL/2)*(NL/2),Blim=8+NL;
+  lc=0,lcmax=2+((N)/(NL*NL)),minstage2=160+((NL)),endsearch=(NL/2)*(NL/2);
+  size_t Blim=8+NL;
 
  cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"\n");goto endl;/*presolved*/}
 print("\nSTART:",mstime()," ms Collisions:",cur,"\n");fflush(stdout);
 //--------Main loop-------------
 
-windowslide();
+first:;
+A=fstgcols(A);
+innerc:;
+if(zerocols(A))A=fstgcols(A);
+second:;lc=0;
+if(cur<Blim)B=rndcell();else{
+do{lc++;
+ //reject cells without collisions(up to a limit)
+B=rndcell2(A,N-A-1);
+}while(lc<lcmax &&zerocols(B));}
+midloop:;
+#ifdef VERBOSE
+info("Midloop:");//midloop test
+#endif
+
+dir=1;swapc(A,B);cur=countudiag();
+fail+=cur>=best;
+if(cur>best){dir=-1;
+swapc(A,B);
+#ifdef VERBOSE
+info("Fail:");//fail test
+#endif
+//if(fail>minstage2)goto first;//;return esearch();
+goto second;}
+tfail+=fail;swapt+=swaps;
+info(cur==best?"Swap0:":"Swap+:");//new iteration update
+fail=cur>=best?fail:0;swaps=0;best=cur;//new record
+if(cur==0){goto fin;}
+if(fail<minstage2)goto innerc;
+//goto first;//return esearch();
+if(cur)goto first;
+fin:;
 print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
 endl:; //end loop
 fflush(stdout);}
