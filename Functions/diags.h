760f20aa4690236e92d7c7e8c75b143952e9919a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 07:25:45 +0300

    Update diags.h

--- Functions/diags.h
+++ Functions/diags.h
@@ -3,5 +3,5 @@ for(size_t i=0;i<len;i++){size_t cur=board[i];
 if(!(i&0xfff)){putchar('+');fflush(stdout);}
  for(size_t z=i+1;z<len;z++){
   size_t zqueen=board[z];
- if(((z-i)==(zqueen-cur))||((z-i)==(cur-zqueen))){return i; };   }  }
+ if(((z-i)==(zqueen-cur))||((z-i)==(cur-zqueen))){return z; };   }  }
 return 0;}
