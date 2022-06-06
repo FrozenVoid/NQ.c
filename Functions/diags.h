293abc62951d3a4fe073c8ad20db07fd26ae2638
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 08:24:11 +0300

    Delete diags.h

--- Functions/diags.h
+++
@@ -1,7 +0,0 @@
-size_t diags(val_t*board,size_t len){//first collision
-for(size_t i=0;i<len;i++){size_t cur=board[i];
-if(!(i&0xfff)){putchar('+');fflush(stdout);}
- for(size_t z=i+1;z<len;z++){
-  size_t zqueen=board[z];
- if(((z-i)==(zqueen-cur))||((z-i)==(cur-zqueen))){return z; };   }  }
-return 0;}
