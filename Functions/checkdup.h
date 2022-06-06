37096202dfd63579dd6b2bb9c704547c899c842b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 07:50:46 +0300

    Update checkdup.h

--- Functions/checkdup.h
+++ Functions/checkdup.h
@@ -10,15 +10,18 @@ print("Press enter to continue/CTRL-C end\n");
 char __attribute__((unused))  tt=getchar();;}}
 if(hasdups && N<1024){printboard('.');}
 memset(diagL,0,N*sizeof(val_t));//zero used
-
+print(hasdups?"":"No duplicates found\n");
 
 }
 void checkdup_end(){int hasdups=0;
-print("\nFinal duplicate check N=",N,"\n");
+print("\nFinal duplicate check N=",N,":");
 combsort(board,N);//data is no longer needed
 for(size_t i=1;i<N;i++){
 if(board[i]==board[i-1]){hasdups=1;print("\nDuplicate column data (",board[i],")at:",i,i-1,"\n");
 print("Press enter to continue/CTRL-C end\n");
 char __attribute__((unused))  tt=getchar();;}
+
+}
 if(hasdups && N<1024){printboard('.');}
-}}
+print(hasdups?"":"No duplicates found\n");
+}
