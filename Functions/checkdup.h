3b5609c28f22015202af43bd9da0a6c50304163b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-06 07:45:29 +0300

    Update checkdup.h

--- Functions/checkdup.h
+++ Functions/checkdup.h
@@ -1,11 +1,24 @@
 void printboard(char sep);
-void checkdup(){int hasdups=0;
-for(size_t i=0;i<N;i++){
-const val_t c=board[i];
-for(size_t z=i+1;z<N;z++){
-if(board[z]==c){hasdups=1;print("\nDuplicate column data (",c,")at:",z,i,"\n");
+void checkdup_file(){int hasdups=0;
+print("Beginning duplicate check:N=",N,"\n");
+//at this point DiagL array is unused.
+memcpy(diagL,board,N*sizeof(val_t));
+combsort(diagL,N);
+for(size_t i=1;i<N;i++){
+if(diagL[i]==diagL[i-1]){hasdups=1;print("\nDuplicate column data (",diagL[i],")at:",i,i-1,"\n");
 print("Press enter to continue/CTRL-C end\n");
 char __attribute__((unused))  tt=getchar();;}}
-}
 if(hasdups && N<1024){printboard('.');}
+memset(diagL,0,N*sizeof(val_t));//zero used
+
+
 }
+void checkdup_end(){int hasdups=0;
+print("\nFinal duplicate check N=",N,"\n");
+combsort(board,N);//data is no longer needed
+for(size_t i=1;i<N;i++){
+if(board[i]==board[i-1]){hasdups=1;print("\nDuplicate column data (",board[i],")at:",i,i-1,"\n");
+print("Press enter to continue/CTRL-C end\n");
+char __attribute__((unused))  tt=getchar();;}
+if(hasdups && N<1024){printboard('.');}
+}}
