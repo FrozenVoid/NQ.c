eb7e26ebee246aafcd33be8e1062ff9695b0b8e4
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 13:18:52 +0300

    Update fstgcols.h

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,7 +2,8 @@
 //return first column(fstcols) if none
 static inline val_t fstgcols(const val_t G){
 size_t i=G+1;
+loop:;
 for(;i<N &&!qccount(i) ;i++);
 
-if(i==N)return fstcols();
+if(i==N){i=0;goto loop;}
 return i;}
