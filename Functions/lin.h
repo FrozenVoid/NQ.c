6fc05f47aa160d08a671d4588e75680238d4cc77
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 10:48:16 +0300

    reset fail at skip and use cubic Blim
    Solved N= 100000000  at: 157083 ms Swaps: 278753334 Fails: 4173645977

--- Functions/lin.h
+++ Functions/lin.h
@@ -4,10 +4,10 @@ void linearsolve(){
  cend=__rdtsc();
  NL=log2index(N);
   lc=0,lcmax=2+((N)/(NL*NL)),minstage2=160+((NL));
-  size_t Blim=8+NL*NL;
+  const size_t Blim=8+NL*NL*NL;
 
  cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"ms\n");goto endl;/*presolved*/}
-print("\nSTART:",mstime()," ms Collisions:",cur,"lcmax:",lcmax,"failmax:",minstage2,"\n");fflush(stdout);
+print("\nSTART:",mstime()," ms Collisions:",cur,"Blim:",Blim,"failmax:",minstage2,"\n");fflush(stdout);
 //--------Main loop-------------
 
 first:;
@@ -36,6 +36,7 @@ info(cur==best?"Swap0:":"Swap+:");//new iteration update
 fail=cur>=best?fail:0;swaps=0;best=cur;//new record
 if(cur==0){goto fin;}
 if(fail<minstage2)goto innerc;
+fail=0;
 if(cur)goto first;
 fin:;
 print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
