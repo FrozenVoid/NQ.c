b692fce4dd653a92c686ef841e6f4921b57e3ca8
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 22:18:00 +0300

    Update solve.h

--- Functions/solve.h
+++ Functions/solve.h
@@ -1,14 +1,15 @@
 //======prefill counts and start solver===
 void solve(void){
 //fill queen location counts
+//set occupied diagonals to 0
 for(size_t i=0;i<N;i++){diagL[board[i]+i]++;
-if(diagL[board[i]+i]>1)asetbit(bitcols,i);
+bitset_set_to_value(bitdiagL,board[i]+i,0);
 }
 for(size_t i=0;i<N;i++){diagR[board[i]+(N-i)]++;
-if(diagR[board[i]+(N-i)]>1)asetbit(bitcols,i);
+bitset_set_to_value(bitdiagR,board[i]+(N-i),0);
 }
-//calculate Sums of collisions
 
+//calculate Sums of collisions
 for(size_t i=0;i<N*2;i++){sumL+=(diagL[i]-1)*(diagL[i]>1);}
 for(size_t i=0;i<N*2;i++){sumR+=(diagR[i]-1)*(diagR[i]>1);}
 linearsolve();}
