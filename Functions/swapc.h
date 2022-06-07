5bd165e78c5f82b2ff532b80f768d14383f953ba
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 09:41:33 +0300

    Update swapc.h

--- Functions/swapc.h
+++ Functions/swapc.h
@@ -1,10 +1,7 @@
  static inline void swapc(val_t x,val_t y){
 val_t  clx,crx,cly,cry;
 tswaps++;swaps+=dir;//valid swaps total
-FETC(&diagL[board[x]+x],1,0);
-FETC(&diagL[board[y]+y],1,0);
-FETC(&diagR[board[x]+(N-x)],1,0);
-FETC(&diagR[board[y]+(N-y)],1,0);
+//prefetch at rndcell()
 clx=diagL[board[x]+x]--;//current X pos Left
 cly=diagL[board[y]+y]--;//current y pos Left
 crx=diagR[board[x]+(N-x)]--;
@@ -18,10 +15,6 @@ sumR-=!!(cry-1);
 //swap
 swapq(x,y);
 //updates sums
-FETC(&diagL[board[x]+x],1,0);
-FETC(&diagL[board[y]+y],1,0);
-FETC(&diagR[board[x]+(N-x)],1,0);
-FETC(&diagR[board[y]+(N-y)],1,0);
 clx=(++diagL[board[x]+x]);
 cly=(++diagL[board[y]+y]);;
 crx=(++diagR[board[x]+(N-x)]);
