8df3c7741f5f322a48ca103dd7b8dcb5dba8ddc3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 22:16:08 +0300

    Update fstgcols.h

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -1,9 +1,11 @@
 //first greater than column(s) with collision
-//return first column(fstcols) if none
 static inline val_t fstgcols(const val_t G){
 size_t i=G+1;
 loop:;
 for(;i<N &&!qccount(i) ;i++);
+if((i==N)){i=0;goto loop;}
 
-if(i==N){i=0;goto loop;}
 return i;}
+
+
+
