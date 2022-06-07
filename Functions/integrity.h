6d6ad9f4a3a08a75854fc7ea376ab8049daaad58
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-07 16:55:19 +0300

    Update integrity.h

--- Functions/integrity.h
+++ Functions/integrity.h
@@ -17,6 +17,8 @@ if(diagR[i]==diagR[i-1]){hasdups=1;hasdupsR=i;print("\nDuplicate right diagonal 
 print("Press enter to continue/CTRL-C end\n");
 char __attribute__((unused))  tt2=getchar();;}}
 
-if(hasdups){print("\nUnsolved collision at diagonalL:",hasdupsL,"diagonalR",hasdupsR,":","Reported Collisions:",sumL+sumR,"\nCtrl-C to end program/enter=exit");fflush(stdout); char __attribute__((unused))  tt3=getchar();exit(32);}
+if(hasdups){print("\nUnsolved collision at diagonalL:",hasdupsL,"diagonalR",hasdupsR,":","Reported Collisions:",sumL+sumR,"\nCtrl-C to end program/enter=exit");fflush(stdout);
+if(N<64)printgraph();
+ char __attribute__((unused))  tt3=getchar();exit(32);}
  print("\nIntegrity checks passed!\n");
 }
