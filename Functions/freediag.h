eb40179609ce8f9f08056e36d02950cb05ca0ded
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-08 07:38:08 +0300

    Update freediag.h

--- Functions/freediag.h
+++ Functions/freediag.h
@@ -1,10 +1,14 @@
 
 static inline int freediag(val_t P,val_t S){//Swapped/Position cells diagonals
+FETC(&diagL[board[S]+P]);
+FETC(&diagR[board[S]+(N-P)]);
+FETC(&diagL[board[P]+S]);
+FETC(&diagR[board[P]+(N-S)]);
 
-const val_t lP=!diagL[board[S]+P];
-const val_t rP=!diagR[board[S]+(N-P)];
-const val_t lS=!diagL[board[P]+S];
-const val_t rS=!diagR[board[P]+(N-S)];
+ val_t lP=!diagL[board[S]+P];
+ val_t rP=!!diagR[board[S]+(N-P)];
+ val_t lS=!diagL[board[P]+S];
+ val_t rS=!!diagR[board[P]+(N-S)];
 return lP+rP+lS+rS;
 ;}
 
