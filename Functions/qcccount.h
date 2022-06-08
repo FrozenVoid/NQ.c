60570e59aed483eba321c4eeea0848ea67498247
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-08 08:28:35 +0300

    Update qcccount.h

--- Functions/qcccount.h
+++ Functions/qcccount.h
@@ -2,4 +2,10 @@
 static inline val_t qccount(val_t P){
 //cannot be zero due being set from q[]
 val_t s=board[P];
-return ((diagL[s+P]))+((diagR[s+(N-P)]))-2;}
+return (((diagL[s+P]))+((diagR[s+(N-P)])))-2;}
+
+static inline val_t qrcount(val_t P){
+//raw count of collisions
+val_t s=board[P];
+return (((diagL[s+P]))+((diagR[s+(N-P)])));}
+
