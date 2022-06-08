e0fe4023156c64a93a24ccdd754310d401d2120b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-08 10:05:30 +0300

    Update fstgcols.h

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,9 +2,8 @@
 static inline val_t fstgcols(const val_t G){
 size_t i=G+1;
 loop:;
-for(;i<N &&!qccount(i) ;i++);
+for(;i<N ;i++){if(qccount(i))return i;}
 if((i==N)){i=0;goto loop;}
-
 return i;}
 
 
