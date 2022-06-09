5ccfd91e3b89896c36466530d2f3ca8087df3feb
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 11:15:52 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,31 +1,24 @@
 //----------------main solver func--------
 void linearsolve(){
-first:;
-A=fstgcols(A);
-innerc:;
-if(zerocols(A))A=fstgcols(A);
+first:; A=fstgcols(A);
+innerc:; if(zerocols(A))A=fstgcols(A);
 second:;
 ///do(fstgcols) while(A==B) prevents infinite loop
 if(unlikely(cur<Blim))B=rndcell();else {do{B=fstgcols(B);}while(unlikely(A==B));}
 //A==B allowed if cur<Blim(to failmax)
-#ifdef VERBOSE
-info("Midloop:");//midloop
-#endif
+verbinfo("Midloop:");//midloop
 setdir(1);swapc(A,B);cur=countudiag();
 if(unlikely(cur>best)){setdir(-1);
-fail++;incfails();
-swapc(A,B);
-#ifdef VERBOSE
-info("Fail:");//fail test
-#endif
+fail++;incfails();swapc(A,B);
+verbinfo("Fail:");//fail test
 goto second;}
-;addswap(swaps);
+addswap(swaps);
 fail=cur>=best?fail:0;
 info("Swap:");//fail==0 -> goodswap
 setswaps(0);
 best=cur;//new record
 if(unlikely(cur==0)){return;}
-if((fail<failmax))goto innerc;
+if(fail<failmax)goto innerc;
 fail=0;//reset fails at late search
 if(cur)goto first;
 }
