140e9723317414dc3949d80454594a06e806ae64
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 21:07:21 +0300

    Update nqbench.sh

--- nqbench.sh
+++ nqbench.sh
@@ -1,3 +1,3 @@
-gcc nq.c -Ofast -march=native -mtune=native -Wall -Wno-unused -pipe -onq -DSILENCE -DNOFILEINPUT -DNOFILEOUTPUT -DNOSCRAMBLE -DNOPRINTBOARD -DNOINTEGRITY
+gcc nq.c -Ofast -march=native -mtune=native -Wall -Wno-unused -pipe -onq -DSILENCE -DNOFILEINPUT -DNOFILEOUTPUT -DNOSCRAMBLE -DNOPRINTBOARD -DNOINTEGRITY -DNOVERIFIER -DNOPRESOLVED
 time ./nq 100000000
 
