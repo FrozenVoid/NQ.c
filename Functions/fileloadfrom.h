99b3afc6d910ab3dbeddfe5e1f4934a54803283e
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 18:52:14 +0300

    support text piped in

--- Functions/fileloadfrom.h
+++ Functions/fileloadfrom.h
@@ -1,12 +1,22 @@
 #ifdef NOFILEINPUT
 #define fileloadfrom(...)
+#define pipestdin(...)
 #else
+void pipestdin(){//assumed to be rows 0..N-1
+setbuf(stdin, NULL);
+for(size_t i=0;i<N;i++){
+uint64_t inp;scanf("%"SCNu64, &inp);
+board[i]=(val_t)inp;}}
+
 void fileloadfrom(char* name){
+if(!strcmp(name,"stdin")){pipestdin();goto filever;}
 FILE* in=fopen(name,"rb");
 if(!in){print("File:",name," cannot be opened");perror("");exit(111);}
 size_t vfqsize=fread(board,sizeof(val_t),N,in);
 if(vfqsize!=N){ print("File:",name,"reading failed at  queen#",vfqsize);perror("");exit(112);}
+fclose(in);
 //verify values in range
+filever:;
 for(size_t i=0;i<N;i++){
 if(board[i]>=N){print("invalid file data at col",i,":",board[i],">=",N);
 exit(99);
@@ -14,5 +24,5 @@ exit(99);
 }}
 
 print("Loaded ",sizeof(val_t),"byte NQ file:",name," N=",N);
-fclose(in);}
+}
 #endif
