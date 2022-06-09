7ef289203d6eb8ee1c9c44600cbeee1c1ff9405a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 14:46:54 +0300

    Imperative control flow tricks to decrease fastpath
    Solved N= 100000000  at: 67259 ms Swaps: 205750334 Fails: 97662916

--- Functions/lin.h
+++ Functions/lin.h
@@ -2,20 +2,29 @@
 void linearsolve(){A=fstgcols(A);
 second:;
 ///do(fstgcols) while(A==B) prevents infinite loop
-if(unlikely(cur<Blim))B=rndcell();else{
+if(unlikely(cur<Blim))goto rndB;
 fstB:;B=fstgcols(B);
-if(unlikely(A==B))goto fstB;}
+if(unlikely(A==B))goto fstB;
+nextt:;
 //A==B allowed if cur<Blim(to failmax)
 verbinfo("Midloop:");//midloop
 swapc(A,B);cur=countudiag();
-if(unlikely(cur>best)){
-fail++;incfails();swapc(A,B);
-verbinfo("Fail:");//fail test
-goto second;}
+if(unlikely(cur>best))goto nfail;
 incswap();fail=cur>=best?fail:0;
 info("Swap:");//fail==0 -> goodswap
 best=cur;//new record
 if(unlikely(cur==0)){return;}
 //next iteration:
-if(fail>=failmax||zerocols(A)){fail=0;A=fstgcols(A);}
-goto second;}
+if(fail>=failmax||zerocols(A)){goto resetA;}
+goto second;
+rndB:;//endsearch Blim reached
+;B=rndcell();
+goto nextt;
+nfail://new fail
+fail++;incfails();swapc(A,B);
+verbinfo("Fail:");//fail test
+goto second;
+resetA:;//reset A
+fail=0;A=fstgcols(A);
+goto second;
+}
