ecf51a566e1593a83f00dfca9510d0a08548a6c5
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 20:39:35 +0300

    Update integrity.h

--- Functions/integrity.h
+++ Functions/integrity.h
@@ -2,27 +2,30 @@
 #define integrity()
 #else
 void integrity(){
-//pedantic checks
 int hasdups=0,hasdupsR=0,hasdupsL=0;
-if(sumL+sumR){print("\nInvalid sumL+sumR to N=",N,"Collisions:",sumL+sumR);fflush(stdout); char __attribute__((unused))  tt2x=getchar();exit(31);}
+if(sumL+sumR){print("\nInvalid sumL+sumR to N=",N,"Collisions:",sumL+sumR);fflush(stdout); ;exit(31);}
 //uses diagX arrays as they are unused
+verbprint("Preparing integrity check for diagonals T:",mstime(),"ms\n");
 for(size_t i=0;i<N;i++){
 diagL[i]=board[i]+i;
 diagR[i]=board[i]+(N-i);}
+verbprint("Sorting diagonals T:",mstime(),"ms\n");
 combsort(diagL,N);
 combsort(diagR,N);
+verbprint("Scanning for duplicate data in DiagL array T:",mstime(),"ms\n");
 for(size_t i=1;i<N;i++){
 if(diagL[i]==diagL[i-1]){hasdups=1;hasdupsL=i;print("\nDuplicate left diagonal  data (",diagL[i],")at:",i,i-1,"\n");
-print("Press enter to continue/CTRL-C end\n");
-char __attribute__((unused))  tt=getchar();;}}
+goto integerror;;}}
+
+verbprint("Scanning for duplicate data in DiagR array T:",mstime(),"ms\n");
 for(size_t i=1;i<N;i++){
 if(diagR[i]==diagR[i-1]){hasdups=1;hasdupsR=i;print("\nDuplicate right diagonal  data (",diagR[i],")at:",i,i-1,"\n");
-print("Press enter to continue/CTRL-C end\n");
-char __attribute__((unused))  tt2=getchar();;}}
+goto integerror;}}
 
+integerror:;
 if(hasdups){print("\nUnsolved collision at diagonalL:",hasdupsL,"diagonalR",hasdupsR,":","Reported Collisions:",sumL+sumR,"\nCtrl-C to end program/enter=exit");fflush(stdout);
 if(N<64)printgraph();
- char __attribute__((unused))  tt3=getchar();exit(32);}
- print("\nIntegrity checks passed!\n");
+;exit(32);}
+ verbprint("\nIntegrity checks passed! T:",mstime(),"ms \n");
 }
 #endif
