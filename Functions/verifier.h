0cb5db76bd7a5f9902297a7c9f778442ad4cec2d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 20:44:56 +0300

    more verbose info

--- Functions/verifier.h
+++ Functions/verifier.h
@@ -1,13 +1,15 @@
 //verify no collisions (diagonals contain Q=1)
 void verifier(){
+verbprint("Scanning for diagonal collisions: T:",mstime(),"ms\n");
 for(size_t i=0;i<N;i++){
-//halt on error(stops nqtest.sh)
 val_t left=(diagL[board[i]+i]);
 val_t right=(diagR[board[i]+(N-i)]);;
 if(left==1 && right==1)continue;
 
-print("Invalid solution to N=",N,"Collision at c,L,R:",i,left,right,"\nPress enter to see board/CTRL-C break\n");
-char __attribute__((unused))  tt=getchar();
-printboard(',');
-return;
-}}
+print("Invalid solution to N=",N,"Collision at c,L,R:",i,left,right,"\n");
+if(N<1024)printboard(',');
+if(N<64)printgraph();
+exit(93);
+}
+verbprint("Verified zero collisions: T:",mstime(),"ms\n");
+}
