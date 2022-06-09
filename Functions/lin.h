e5b96bba07131735cb56b5477ef76453435b712d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 11:04:47 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,17 +1,5 @@
 //----------------main solver func--------
 void linearsolve(){
- cend=__rdtsc();
- NL=log2index(N);
-  lc=0,lcmax=2+((N)/(NL*NL)),failmax=160+((NL));
-  const size_t Blim=8+NL*NL*NL;
-
- cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"ms\n");goto endl;/*presolved*/}
-print("\nSTART:",mstime()," ms Collisions:",cur,"Blim:",Blim,"failmax:",failmax,"\n");fflush(stdout);
-
-fastdiagswap();
-#ifdef VERBOSE
-print("Linear:",mstime(),"ms\n");
-#endif
 first:;
 A=fstgcols(A);
 innerc:;
@@ -36,17 +24,11 @@ fail=cur>=best?fail:0;
 info("Swap:");//fail==0 -> goodswap
 setswaps(0);
 best=cur;//new record
-if(unlikely(cur==0)){goto fin;}
+if(unlikely(cur==0)){return;}
 if((fail<failmax))goto innerc;
 fail=0;//reset fails at late search
 if(cur)goto first;
-fin:;
-#ifndef SILENCE
-print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
-#else
-print("\nSolved N=",N," at:",mstime(),"ms\n");
-#endif
-endl:;fflush(stdout);}
+}
 
 
 
