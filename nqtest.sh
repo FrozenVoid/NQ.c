5241fe93f885ae6132b9f5942736571a5ee3ef28
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 20:37:10 +0300

    upgrade to any parameter nqtest.sh

--- nqtest.sh
+++ nqtest.sh
@@ -1,6 +1,10 @@
+#pass any parameters to nq $*
 gcc nq.c -gdwarf -Ofast -march=native -mtune=native -pipe -onq -DVERBOSE
-for (( n=4;n<11111111;n++ ))
+for (( n=8;n<11111111;n++ ))
 do
-  echo "Test N-Queens= $n  "
-   ./nq $n c
+  echo "============Test N-Queens= $n ======="
+   ./nq $n $*
+   ecode=$?
+#halt on non-zero code
+   [ $ecode -ne 0 ] && exit
 done
