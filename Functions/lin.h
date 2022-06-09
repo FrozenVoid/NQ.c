4f6e8a35e7da1e01e6de97336d7503bd28844ff3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 08:17:00 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -33,15 +33,17 @@ if(fastlim--)goto fastdiag_start;
 void linearsolve(){
  cend=__rdtsc();
  NL=log2index(N);
-  lc=0,lcmax=2+((N)/(NL*NL)),minstage2=160+((NL));
+  lc=0,lcmax=2+((N)/(NL*NL)),failmax=160+((NL));
   const size_t Blim=8+NL*NL*NL;
 
  cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"ms\n");goto endl;/*presolved*/}
-print("\nSTART:",mstime()," ms Collisions:",cur,"Blim:",Blim,"failmax:",minstage2,"\n");fflush(stdout);
+print("\nSTART:",mstime()," ms Collisions:",cur,"Blim:",Blim,"failmax:",failmax,"\n");fflush(stdout);
 //--------Main loop-------------
 
 fastdiagswap();
+#ifdef VERBOSE
 print("Linear:",mstime(),"ms\n");
+#endif
 first:;
 A=fstgcols(A);
 innerc:;
@@ -64,8 +66,7 @@ tfail+=fail;swapt+=swaps;
 info(cur==best?"Swap0:":"Swap+:");//new iteration update
 fail=cur>=best?fail:0;swaps=0;best=cur;//new record
 if(cur==0){goto fin;}
-if(fail<minstage2)goto innerc;
-fail=0;
+if(fail<failmax)goto innerc;
 if(cur)goto first;
 fin:;
 print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
