5b45684ecd8ad1dc7a8ac3fcc7d5986d8bd79a1f
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 20:22:04 +0300

    more timing info

--- Functions/checkdup.h
+++ Functions/checkdup.h
@@ -3,17 +3,20 @@
 #else
 void printboard(char sep);
 void checkdup(){int hasdups=0;
-print("Beginning duplicate check:N=",N,"\n");
+verbprint("Beginning duplicate check:N=",N,"  T:",mstime(),"ms\n");
 //at this point DiagL array is unused/unset
 memcpy(diagL,board,N*sizeof(val_t));
+verbprint("Copied temporary array  T:",mstime(),"ms\n");
 combsort(diagL,N);
+verbprint("Sorted temporary array  T:",mstime(),"ms\n");
 for(size_t i=1;i<N;i++){
-if(diagL[i]==diagL[i-1]){hasdups=1;print("\nDuplicate column data (",diagL[i],")at:",i,i-1,"\n");
+if(diagL[i]!=diagL[i-1])continue;
+{hasdups=1;print("\nDuplicate column data (",diagL[i],")at:",i,i-1,"\n");
 if(hasdups && N<1024){printboard('.');puts("");}
 exit(231);;}}
 
 memset(diagL,0,N*sizeof(val_t));//zero used
-print("No duplicates found\n");
+verbprint("No duplicates found  T:",mstime(),"ms\n");
 
 }
 #endif
