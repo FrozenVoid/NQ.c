ff90116b0beba4bad302504e8c0d333049706f63
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 15:07:45 +0300

    Update fstgcols.h

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -1,7 +1,7 @@
 //first greater than column(s) with collision
 static inline val_t fstgcols(const val_t G){
 size_t i=G+1;fstgcolsloop:;
-for(;unlikely(i<N)&& likely(zerocols(i));i++);
+for(;unlikely(i<N)&& likely(zerocols2(i));i++);
 if(i==N){i=0;goto fstgcolsloop;}
 return i;}
 
