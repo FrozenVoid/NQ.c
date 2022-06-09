dccd7155a9f9547c782eb19817b899a2f31049f3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 11:40:33 +0300

    merge innerc/second
    Solved N= 100000000  at: 70599 ms Swaps: 205741775 Fails: 97639978

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,7 +1,5 @@
 //----------------main solver func--------
-void linearsolve(){
-first:; A=fstgcols(A);
-innerc:; if(zerocols(A))A=fstgcols(A);
+void linearsolve(){first:; A=fstgcols(A);
 second:;
 ///do(fstgcols) while(A==B) prevents infinite loop
 if(unlikely(cur<Blim))B=rndcell();else {do{B=fstgcols(B);}while(unlikely(A==B));}
@@ -12,17 +10,12 @@ if(unlikely(cur>best)){setdir(-1);
 fail++;incfails();swapc(A,B);
 verbinfo("Fail:");//fail test
 goto second;}
-addswap(swaps);
-fail=cur>=best?fail:0;
+addswap(swaps);fail=cur>=best?fail:0;
 info("Swap:");//fail==0 -> goodswap
-setswaps(0);
-best=cur;//new record
+setswaps(0);best=cur;//new record
 if(unlikely(cur==0)){return;}
-if(fail<failmax)goto innerc;
+if(fail<failmax){
+A=zerocols(A)?fstgcols(A):A;
+goto second;}
 fail=0;//reset fails at late search
-if(cur)goto first;
-}
-
-
-
-
+if(cur)goto first;}
