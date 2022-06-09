9eb9f39e9dce251e6f89a51fca5100c4c33ae79e
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 09:21:21 +0300

    branch hints speedup
    Solved N= 100000000  at: 76720 ms Swaps: 205462064 Fails: 1120637037

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,7 +2,7 @@
 static inline val_t fstgcols(const val_t G){
 size_t i=G+1;
 loop:;
-for(;i<N&& qrcount(i)==2;i++);
+for(;unlikely(i<N)&& likely(qrcount(i)==2);i++);
 if((i==N)){i=0;goto loop;}
 return i;}
 
