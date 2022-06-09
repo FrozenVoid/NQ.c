40416d9fdbc7e82a51de97d2553a14be5b914df0
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 22:47:22 +0300

    Update solve.h

--- Functions/solve.h
+++ Functions/solve.h
@@ -1,9 +1,11 @@
 //======prefill counts and start solver===
 void solve(void){
+if(N==2||N==3){print("No solution for N=",N,"\n");exit(0);}
 //fill queen location counts
+verbprint("Filling diagonals: T:",mstime(),"ms\n");
 for(size_t i=0;i<N;i++){diagL[board[i]+i]++;}
 for(size_t i=0;i<N;i++){diagR[board[i]+(N-i)]++;}
-
+verbprint("Summing diagonals: T:",mstime(),"ms\n");
 //calculate Sums of collisions
 for(size_t i=0;i<N*2;i++){sumL+=(diagL[i]-1)*(diagL[i]>1);}
 for(size_t i=0;i<N*2;i++){sumR+=(diagR[i]-1)*(diagR[i]>1);}
