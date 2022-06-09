79357e6779f119996b38b8a1f10d04634129da57
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 14:07:21 +0300

    Update solve.h

--- Functions/solve.h
+++ Functions/solve.h
@@ -10,7 +10,7 @@ for(size_t i=0;i<N*2;i++){sumR+=(diagR[i]-1)*(diagR[i]>1);}
 
  cend=__rdtsc();
  NL=log2index(N);
-  lc=0,lcmax=2+((N)/(NL*NL)),failmax=160+((NL));
+  failmax=160+NL;
   Blim=8+NL*NL*NL;
 
  cur=countudiag(),best=cur;if(cur==0){print("\nPre-Solved N=",N," at:",mstime(),"ms\n");goto endl;/*presolved*/}
