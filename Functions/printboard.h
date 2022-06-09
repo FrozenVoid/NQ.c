228e0ca750c4514e62df26a9fc69b453a22398db
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 16:56:17 +0300

    Update printboard.h

--- Functions/printboard.h
+++ Functions/printboard.h
@@ -1,3 +1,7 @@
+#ifdef NOBOARDOUTPUT
+#define printboard() ;
+#define printgraph() ;
+#else
 void printboard(char sep){
  printf("\n%"PRIu64"",(uint64_t)board[0]);
 for(size_t i=1;i<N;i++){putchar(sep);
@@ -10,3 +14,4 @@ for(i64 zz=N-1;zz>-1;zz--){//row N-1/0
 for(i64 k=0;k<N;k++){
 printf(zz==board[k]?"Q|":"_|");}puts("");}
 }
+#endif
