ee9a3699a3c3aacb54c79ed295ef1d6eca84036e
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 19:02:07 +0300

    also print graphical board if N<64 and verbose mode

--- Functions/info.h
+++ Functions/info.h
@@ -6,5 +6,9 @@ void info(char* data){
   if(__rdtsc()-cend>NCYCLES  ){
   clock_t Ntime=mstime();
   print("\n",data," cols=",best,"A=",A,"B=",B,"fails:",(fail),"\nswap:",1.0*swapt/Ntime,"fail:",1.0*tfail/Ntime);
-  print("\nT:",Ntime,"ms Col%",100.0*(cur)/N,"Swapt",swapt,"Valid%",100.0*swapt/tswaps);cend=__rdtsc();fflush(stdout);}}
+  print("\nT:",Ntime,"ms Col%",100.0*(cur)/N,"Swapt",swapt,"Valid%",100.0*swapt/tswaps);cend=__rdtsc();
+#ifdef VERBOSE
+if(N<64)printgraph();
+#endif
+  fflush(stdout);}}
 #endif
