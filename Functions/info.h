002aba7d8be103691e0aa7b56e7e7f77e84fa427
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 12:45:43 +0300

    Update info.h

--- Functions/info.h
+++ Functions/info.h
@@ -2,9 +2,9 @@
 #ifdef SILENCE
 #define info(...)
 #else
- void info(char* data){
+void info(char* data){
   if(__rdtsc()-cend>NCYCLES  ){
   clock_t Ntime=mstime();
-  print("\n",data," cols=",best,"A=",A,"B=",B,"valid/fail:",swaps,"/",(fail),"\nswap:",1.0*swapt/Ntime,"fail:",1.0*tfail/Ntime);
+  print("\n",data," cols=",best,"A=",A,"B=",B,"fails:",(fail),"\nswap:",1.0*swapt/Ntime,"fail:",1.0*tfail/Ntime);
   print("\nT:",Ntime,"ms Col%",100.0*(cur)/N,"Swapt",swapt,"Valid%",100.0*swapt/tswaps);cend=__rdtsc();fflush(stdout);}}
 #endif
