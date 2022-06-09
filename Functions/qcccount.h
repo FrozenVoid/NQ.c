a456e2f9bdcba193bdcba11f59971b4701aeb44a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-09 15:07:25 +0300

    Update qcccount.h

--- Functions/qcccount.h
+++ Functions/qcccount.h
@@ -1,3 +1,7 @@
+static inline int zerocols2(const val_t P){
+const val_t s=board[P];
+return ((diagL[s+P])+diagR[s+(N-P)])==2;
+}
 //queen collisons at position: 2=none,2+=collision
 static inline val_t qccount(val_t P){
 //cannot be zero due being set from q[]
