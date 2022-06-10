90164fa2ca479123da5d4661f7ba73b6dd17b8c7
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 20:36:27 +0300

    branch hints win over clever booleans
    Solved N= 100000000  in  39667 ms(linear solver only)

--- Functions/swapc.h
+++ Functions/swapc.h
@@ -13,10 +13,10 @@ cry=diagR[board[y]+(N-y)]--;
 
 //reduce sums,first,last
 //if clx==1 0 clx>1=1  (clx-1)=0
-sumL-=!!(clx-1);//reduce sum if old collision
-sumR-=!!(crx-1);
-sumL-=!!(cly-1);
-sumR-=!!(cry-1);
+if(likely((clx-1)))sumL--;
+if(likely((crx-1)))sumR--;
+if(likely((cly-1)))sumL--;
+if(likely((cry-1)))sumR--;
 //swap
 swapq(x,y);
 
@@ -25,10 +25,11 @@ clx=(++diagL[board[x]+x]);
 cly=(++diagL[board[y]+y]);;
 crx=(++diagR[board[x]+(N-x)]);
 cry=(++diagR[board[y]+(N-y)]);
+//0 1+
+if(likely((clx-1)))sumL++;
+if(likely((crx-1)))sumR++;
+if(likely((cly-1)))sumL++;
+if(likely((cry-1)))sumR++;
 
-sumL+=!!(clx-1);//reduce sum if old collision
-sumR+=!!(crx-1);
-sumL+=!!(cly-1);
-sumR+=!!(cry-1);
 
 }
