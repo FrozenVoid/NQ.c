b8b3e9db62e6c94d0d2a98930f6be8284f51764d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 21:31:37 +0300

    remove const from G(greater than) parameter
    Solved N= 100000000  in  38549 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -1,5 +1,5 @@
 //first greater than column(s) with collision
-static inline val_t fstgcols(const val_t G){
+static inline val_t fstgcols( val_t G){
 size_t i=G+1;const size_t maxN=N;;
 fstgcolsloop:;
 for(;unlikely(i<maxN);i++){
