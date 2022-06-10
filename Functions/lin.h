31fd81cd1cb1744afba6aa98153e7573bc4c33cf
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 18:07:18 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -2,7 +2,8 @@
 void linearsolve(){if(!cur)return;
 A=fstgcols(A);second:;
 if(unlikely(cur<Blim))goto rndB;
-B=fstgcols(B);nextt:;
+fstB:;B=fstgcols(B);
+nextt:;
 verbinfo("Midloop:");//midloop
 swapc(A,B);cur=countudiag();
 if(unlikely(cur>best))goto nfail;
@@ -11,16 +12,20 @@ info("Swap:");//fail==0 -> goodswap
 best=cur;//new record
 if(unlikely(cur==0)){return;}
 //next iteration:
-if(fail>=failmax||zerocols2(A)){goto resetA;}
+if(unlikely(fail>=failmax)||zerocols2(A)){goto resetA;}
+
 goto second;
 //control flow: jumps are cheap.
 rndB:;
 B=(rndedgecell(A));
 goto nextt;
+
 nfail://new fail
 fail++;incfails();swapc(A,B);
 verbinfo("Fail:");//fail test
+
 goto second;
+
 resetA:;//reset A
 fail=0;A=fstgcols(A);
 goto second;
