5eaf11b7a78c4468ceee8ce0f089a11d38aa2b62
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 21:26:37 +0300

    inverse branch hint to i<N? faster!
    Solved N= 100000000  in  38688 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -1,7 +1,7 @@
 //first greater than column(s) with collision
 static inline val_t fstgcols(const val_t G){
 val_t i=G+1;fstgcolsloop:;
-for(;likely(i<N);i++){
+for(;unlikely(i<N);i++){
 if((qrcount(i)!=2))return i;};
 if((i==N)){i=0;goto fstgcolsloop;}
 return i;}
