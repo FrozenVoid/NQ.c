17e2be0e08fd2cea4abb37a7fb372109b6591cce
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 06:31:28 +0300

    Delete modreduce.h

--- Functions/modreduce.h
+++
@@ -1,3 +0,0 @@
-//https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
- static inline val_t modreduce(val_t x, val_t N) {
-return ((sval_t) x * (sval_t) N) >> (sizeof(val_t)*8);}
