d6f56698864a891ec016b9a16ec2b5c5fb74dc3c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 10:08:23 +0300

    Update random.h

--- standalone/random.h
+++ standalone/random.h
@@ -5,6 +5,8 @@ rnd(seed) return randomized seed->rndnum
 rnd1() get random 64bit num
 range(max) ^below max(0-max)
 range32(max) same but for 32-bit(faster)
+checksum(u64* arr,len) checksum for array
+stringsum(char* arr,len) checksum for string
 
 */
 #include <stdint.h>
@@ -13,15 +15,30 @@ static inline uint64_t rotl(const uint64_t x, int k) {
 }
 
 static inline uint64_t rnd(uint64_t a){
-a+=rotl(~a,7)^rotl(a,11);
-a-=rotl(a,6)^rotl(~a,19);
-return a;}
+static uint64_t a=a,b=~a+17;
+a+=rotl(b,17)^rotl(a,3);
+b+=rotl(a,13)^rotl(b,8);
+return b;}
 
 static inline uint64_t rnd1(){
-static uint64_t a=17;
-a+=rotl(~a,7)^rotl(a,11);
-a-=rotl(a,6)^rotl(~a,19);
-return a;}
+static uint64_t a=17,b=~1;
+a+=rotl(b,17)^rotl(a,3);
+b+=rotl(a,13)^rotl(b,8);
+return b;}
+
+//can be used as hash
+static inline uint64_t checksum( uint64_t*arr,const size_t len){uint64_t res=0;
+for(size_t i=0;i<len;i++)res+=rnd(res+arr[i]);
+return res;
+}
+static inline uint64_t stringsum( char*arr,const size_t len){uint64_t res=0;
+const size_t len8=len/8;
+uint64_t*arr2=(uint64_t*)arr;
+if(len8)res+=checksum(arr2,len8);
+for(size_t i=len8*8;i<len;i++)res+=rnd(res+arr[i]);
+
+return res;
+}
 
 static inline uint64_t range(uint64_t maxval){
 uint64_t a=rnd1();
