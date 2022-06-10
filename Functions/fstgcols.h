4b958bb2605495f4fa011e370d27ba14f9a79d0f
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 22:49:09 +0300

    early midloop return
    Solved N= 100000000  in  38122 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -3,7 +3,7 @@ static inline const val_t fstgcols( val_t G){
 size_t i=G+1;const size_t maxN=N;;
 fstgcolsloop:;
 if(unlikely(i<maxN)){
-if(likely(zerocols2(i))){i++;goto fstgcolsloop;}}
+if(likely(zerocols2(i))){i++;goto fstgcolsloop;} return i;}
 
 if((i==maxN)){i=0;goto fstgcolsloop;}
 return i;}
