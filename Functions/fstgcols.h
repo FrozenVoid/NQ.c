3b8592ccab22bb3f86ffd4bf7f185d4a9cb8d233
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 14:23:43 +0300

    use qccount
    Switching to system time reporting:
    real	0m32.637s
    user	0m32.164s
    sys	0m0.268s

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -3,6 +3,7 @@ static inline const val_t fstgcols( val_t G){
 size_t i=G+1, maxN=N;;
 fstgcolsloop:;
 if(likely(i<maxN)){
-if(likely(zerocols2(i))){i++;goto fstgcolsloop;} return i;}else
+if(unlikely(qccount(i)))return i;
+i++;goto fstgcolsloop;}else
 if(likely(i==maxN)){i=0;goto fstgcolsloop;}
 return i;/*compiler likes end return struct*/}
