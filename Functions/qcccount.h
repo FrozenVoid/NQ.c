6b830f25282850679b99a3b72e0d59013c62f063
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 14:21:07 +0300

    use size_t for index

--- Functions/qcccount.h
+++ Functions/qcccount.h
@@ -5,7 +5,7 @@ return likely(((diagL[s+P])+diagR[s+(N-P)])==2);
 //queen collisons at position: 2=none,2+=collision
 static inline val_t qccount(val_t P){
 //cannot be zero due being set from q[]
-val_t s=board[P];
+const size_t s=board[P];
 return (((diagL[s+P]))+((diagR[s+(N-P)])))-2;}
 
 static inline val_t qrcount(val_t P){
