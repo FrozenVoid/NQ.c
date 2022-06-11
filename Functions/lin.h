430bc09eca4a43c2774bfd55031bc5a8c25f49e8
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 17:35:23 +0300

    use separate swaps for each stage3
    Solved N= 100000000  in  32268 ms(linear solver only)

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,21 +1,28 @@
-//----------------Stage 3--------
 void NOINLINE linearsolveend(){if(!cur)return;
 esecond:;B=(rndedgecell(A));
-emidloop:;verbinfo("EMidloop:");//midloop
-swapc(A,B);incswap();
+emidloop:;
+verbinfo("EMidloop:");//midloop
+swapc(A,B);cur=countudiag();
+if(unlikely(cur<best))goto egoodswap;
+fail++;incfails();verbinfo("EFail:");//fail test
+if(unlikely(fail>=failmax)){fail=0;best=cur;goto eresetA;}
+goto swapfix;egoodswap:;
+incswap();fail=0;
 info("ESwap:");//fail==0 -> goodswap
+best=cur;//new record
 if(unlikely(cur==0)){return;}
 //next iteration:
 if(unlikely(zerocols2(A))){goto eresetA;;}
+
 goto esecond;
 eresetA:;A=fstgcols(A);goto emidloop;
+swapfix:;swapc(A,B); goto esecond;
 
 }
-
 //--------------------Stage2---------------
 void NOINLINE linearsolve(){if(!cur)return;
 goto resetA;midloop:;verbinfo("Midloop:");
-swapc(A,B);//badswap rejected at swapc.h
+swapauto(A,B);//badswap rejected at swapc.h
 //goodswap:
 goodswap:;incswap();
 info("Swap:");best=cur;//new record
