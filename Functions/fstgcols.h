7165d3c37f233d6dee3a8f4189284132dd779f80
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 09:14:59 +0300

    merge both if's into else if
    Solved N= 100000000  in  31694 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -3,7 +3,6 @@ static inline const val_t fstgcols( val_t G){
 size_t i=G+1; size_t maxN=N;;
 fstgcolsloop:;
 if(likely(i<maxN)){
-if(likely(zerocols2(i))){i++;goto fstgcolsloop;} return i;}
-
+if(likely(zerocols2(i))){i++;goto fstgcolsloop;} return i;}else
 if(likely(i==maxN)){i=0;goto fstgcolsloop;}
 return i;/*compiler likes end return struct*/}
