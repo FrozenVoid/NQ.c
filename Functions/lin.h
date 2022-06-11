1dbfc7a709e8e4aa417dbf18d05cb7979e4adf26
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 18:04:21 +0300

    rem countudiag

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,8 +1,9 @@
 void NOINLINE linearsolveend(){if(!cur)return;
+verbprint("Stage3:",mstime(),"ms\n");
 esecond:;B=(rndedgecell(A));
 emidloop:;
 verbinfo("EMidloop:");//midloop
-swapc(A,B);cur=countudiag();
+swapc(A,B);
 if(unlikely(cur<best))goto egoodswap;
 fail++;incfails();verbinfo("EFail:");//fail test
 if(unlikely(fail>=failmax)){fail=0;best=cur;goto eresetA;}
@@ -21,13 +22,15 @@ swapfix:;swapc(A,B); goto esecond;
 }
 //--------------------Stage2---------------
 void NOINLINE linearsolve(){if(!cur)return;
+verbprint("Stage2:",mstime(),"ms\n");
 goto resetA;midloop:;verbinfo("Midloop:");
+if(unlikely(cur<Blim))return linearsolveend();
 swapauto(A,B);//badswap rejected at swapc.h
 //goodswap:
 goodswap:;incswap();
 info("Swap:");best=cur;//new record
-if(unlikely(cur<Blim))return linearsolveend();
-if(zerocols2(A)){
+
+if(zerocols2(A)){verbinfo("ResetA");
 resetA:;A=fstgcols(A);goto midloop;}
 B=fstgcols(B);goto midloop;
 }
