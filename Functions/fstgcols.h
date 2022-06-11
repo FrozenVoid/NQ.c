385fda0d3334e0de22a3ae5f3ad06b96ded96e85
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 09:07:23 +0300

    switch branch hint
    Solved N= 100000000  in  31733 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,7 +2,7 @@
 static inline const val_t fstgcols( val_t G){
 size_t i=G+1;const size_t maxN=N;;
 fstgcolsloop:;
-if(unlikely(i<maxN)){
+if(likely(i<maxN)){
 if(likely(zerocols2(i))){i++;goto fstgcolsloop;} return i;}
 
 if(likely(i==maxN)){i=0;goto fstgcolsloop;}
