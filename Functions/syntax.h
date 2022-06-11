76b8d1a85cf76c9cd2e7a82597dc1376a4164223
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 07:55:12 +0300

    support NOSYNTAX switch

--- Functions/syntax.h
+++ Functions/syntax.h
@@ -1,3 +1,7 @@
+#ifdef NOSYNTAX
+#define syntax(...) exit(1)
+#else
 void syntax(){puts("Syntax:nq N [p|f|t|i] [filename|sep]\n N=Board size \n p [string]=printboard [separator] \n f=write result as file \nt=test presolved array\n i filename=load u32/u64 queen array filename\n Xs =scramble rows X times(N*X)\n");exit(1);
 
 }
+#endif
