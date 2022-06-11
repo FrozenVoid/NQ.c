62ae159e3b6376ed78e565736437054f88a36d7a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 20:17:22 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,24 +1,4 @@
-void NOINLINE edgecellsolver(){if(!cur)return;
-//failmax;
-print("\nStage3:",mstime(),"ms\n");
-esecond:;B=rndcell();
-emidloop:;verbinfo("EMidloop:");//midloop
-swapc(A,B);//cur update in swapc()
-if(unlikely(cur<best))goto egoodswap;
-fail++;
-incfails();
-verbinfo("EFail:");//fail test
-if(unlikely(fail>=failmax)){fail=0;best=cur;goto eresetA;}
-goto swapfix;egoodswap:;
-incswap();fail=0;info("ESwap:");
-if(unlikely(cur==0)){return;}
-best=cur;//new record
-if(unlikely(zerocols2(A))){goto eresetA;;}
-goto esecond;//next iteration:
-eresetA:;A=fstgcols(A);goto emidloop;
-swapfix:;swapc(A,B); goto esecond;
 
-}
 //--------------------Stage2---------------
 void NOINLINE linearsolve(){
 const val_t stage3lim=119+N/512;
