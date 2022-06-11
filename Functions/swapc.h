e24523175ffbe6709efed01726107c2ea8c13949
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-11 17:34:45 +0300

    dedicated swapauto function for stage2

--- Functions/swapc.h
+++ Functions/swapc.h
@@ -1,5 +1,5 @@
 
- static inline void swapc(val_t x,val_t y){
+ static inline void swapauto(val_t x,val_t y){
  if(unlikely(x==y))return;
  swapreset:;
 val_t clx,crx,cly,cry;//current:right/left:x/y
@@ -33,6 +33,42 @@ if(unlikely((cly-1)))sumL++;
 if(unlikely((cry-1)))sumR++;
 
 cur=countudiag();
-if((cur>best))goto swapreset;
-best=cur;//update best
+if(cur>best){goto swapreset;}
+
+}
+
+
+static inline void swapc(val_t x,val_t y){
+ if(unlikely(x==y))return;
+val_t clx,crx,cly,cry;//current:right/left:x/y
+#ifndef SILENCE
+tswaps++;// swaps total(all)
+#endif
+
+clx=diagL[board[x]+x]--;//current X pos Left
+cly=diagL[board[y]+y]--;//current y pos Left
+crx=diagR[board[x]+(N-x)]--;
+cry=diagR[board[y]+(N-y)]--;
+
+//reduce sums,first,last
+//if clx==1 0 clx>1=1  (clx-1)=0
+if(unlikely((clx-1)))sumL--;
+if(unlikely((crx-1)))sumR--;
+if(unlikely((cly-1)))sumL--;
+if(unlikely((cry-1)))sumR--;
+//swap
+swapq(x,y);
+
+//updates sums
+clx=(++diagL[board[x]+x]);
+cly=(++diagL[board[y]+y]);;
+crx=(++diagR[board[x]+(N-x)]);
+cry=(++diagR[board[y]+(N-y)]);
+//0 1+
+if(unlikely((clx-1)))sumL++;
+if(unlikely((crx-1)))sumR++;
+if(unlikely((cly-1)))sumL++;
+if(unlikely((cry-1)))sumR++;
+
+
 }
