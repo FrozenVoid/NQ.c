a02abaf09ae8d09bce04755c7fe7f2b7234cf916
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 21:38:00 +0300

    inline qccount
    Solved N= 100000000  in  92243 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -3,7 +3,10 @@ static inline const val_t fstgcols( val_t G){
 size_t i=G+1, maxN=N;;
 fstgcolsloop:;
 if(likely(i==maxN)){i=0;goto fstgcolsloop;}
-if(unlikely(qccount(i)))return i;
+const size_t s=board[i];
+__builtin_prefetch(&diagL[s+i],0,0);
+__builtin_prefetch(&diagR[s+(maxN-i)],0,0);
+if(unlikely((((diagL[s+i]))+((diagR[s+(maxN-i)])))-2))return i;
 i++;goto fstgcolsloop;
 return i;/*compiler likes end return struct*/}
 
