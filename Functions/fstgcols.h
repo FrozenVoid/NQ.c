5f6909b854b03493058afef18421adc47526b9b0
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 21:15:14 +0300

    invert loop and simplify it
    Solved N= 100000000  in  97538 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,9 +2,8 @@
 static inline const val_t fstgcols( val_t G){
 size_t i=G+1, maxN=N;;
 fstgcolsloop:;
-if(likely(i<maxN)){
+if(unlikely(i==maxN)){i=0;goto fstgcolsloop;}
 if(unlikely(qccount(i)))return i;
-i++;goto fstgcolsloop;}else
-if(likely(i==maxN)){i=0;goto fstgcolsloop;}
+i++;goto fstgcolsloop;
 return i;/*compiler likes end return struct*/}
 
