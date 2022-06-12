53b26784d95febb0d960258837049444f0a77673
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 18:21:48 +0300

    Update edgesolver.h

--- Functions/edgesolver.h
+++ Functions/edgesolver.h
@@ -4,19 +4,13 @@ if(!cur)return;
 print("\nStage3:",mstime(),"ms failmax:",edgefailmax,"\n");
 esecond:;B=rndedgecell(A);
 emidloop:;verbinfo("EMidloop:");//midloop
-swapc(A,B);//cur update in swapc()
-if(unlikely(cur<best))goto egoodswap;
-fail++;
-incfails();verbinfo("EFail:");//fail test
-if(unlikely(fail>=edgefailmax)){fail=0;best=cur;goto eresetA;}
-
-goto swapfix;egoodswap:;
-incswap();fail=0;info("ESwap:");
+swapauto(A,B);//cur update in swapc()
+info("ESwap:");
 if(unlikely(cur==0)){return;}
 best=cur;//new record
-if(unlikely(zerocols2(A))){goto eresetA;;}
+if(unlikely(zerocols2(A))||fail>edgefailmax){fail*=fail<edgefailmax;goto eresetA;;}
 goto esecond;//next iteration:
 eresetA:;A=fstgcols(A);goto emidloop;
-swapfix:;swapc(A,B); goto esecond;
+
 
 }
