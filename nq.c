b276c28f16142637087a218acf94d6614db49589
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 19:30:57 +0300

    Update nq.c

--- nq.c
+++ nq.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <x86intrin.h>//__rdtsc
-#include "standalone/print.h"
-#include "standalone/random.h"
-#include "standalone/combsort.h"
-#include "standalone/hitstat.h"
+#include "standalone/print.h"//required
+#include "standalone/random.h"//required
+#include "standalone/combsort.h"//for checks
+#include "standalone/hitstat.h"//optional
 
 #include "Functions/macros.h"
 #include "Functions/globals.h"
 
 
-#include "Functions/checkdup.h"
+#include "Functions/checkdup.h"//-DDUPCHECK
 #include "Functions/qcccount.h"
 
-#include "Functions/printboard.h"
-#include "Functions/fileboard.h"
-#include "Functions/fileloadfrom.h"
+#include "Functions/printboard.h"//-DNOPRINTBOARD
+#include "Functions/fileboard.h"// -DNOFILEOUTPUT
+#include "Functions/fileloadfrom.h"//-DNOFILEINPUT
 #include "Functions/fstgcols.h"
-#include "Functions/info.h"
+#include "Functions/info.h"// -DSILENCE
 #include "Functions/swapauto.h"
 
 #include "Functions/edgesolver.h"
 #include "Functions/lin.h"
 #include "Functions/solve.h"
-#include "Functions/integrity.h"
-#include "Functions/presolve.h"
-#include "Functions/scramble.h"
-#include "Functions/verifier.h"
-#include "Functions/syntax.h"
+#include "Functions/integrity.h"// -DNOINTEGRITY
+#include "Functions/presolve.h"// -DNOPRESOLVED
+#include "Functions/scramble.h"// -DNOSCRAMBLE
+#include "Functions/verifier.h"// -DNOVERIFIER
+#include "Functions/syntax.h"// -DNOSYNTAX
 #include "Functions/main.h"
