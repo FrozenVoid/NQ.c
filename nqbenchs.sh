8c6d47fcdd99921e063e6744bb31efddf047fd58
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 18:40:04 +0300

    Introduce scram bench

---
+++ nqbenchs.sh
@@ -0,0 +1,15 @@
+gcc nq.c -Ofast -march=native -mtune=native -Wall -Wno-unused -pipe -onq -DSILENCE -DNOFILEINPUT -DNOFILEOUTPUT -DNOPRINTBOARD -DNOINTEGRITY -DNOVERIFIER -DNOPRESOLVED -DNDEBUG -DNOSYNTAX
+function scramtest(){
+for (( n=0;n<10;n++ ))
+  do
+     echo "==========Bench Scram= $n s ======="
+     echo "./nq 10000000 $n s"
+     ./nq 10000000 "$n s"
+     ecode=$?
+#halt on non-zero code
+     [ $ecode -ne 0 ] && exit
+  done
+}
+time scramtest
+
+
