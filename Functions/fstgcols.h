e78f334dae6f1008cdd58ff341658f50248490fa
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 21:20:53 +0300

    fastpath for i==maxN
    Solved N= 100000000  in  94172 ms(linear solver only)

--- Functions/fstgcols.h
+++ Functions/fstgcols.h
@@ -2,7 +2,7 @@
 static inline const val_t fstgcols( val_t G){
 size_t i=G+1, maxN=N;;
 fstgcolsloop:;
-if(unlikely(i==maxN)){i=0;goto fstgcolsloop;}
+if(likely(i==maxN)){i=0;goto fstgcolsloop;}
 if(unlikely(qccount(i)))return i;
 i++;goto fstgcolsloop;
 return i;/*compiler likes end return struct*/}
