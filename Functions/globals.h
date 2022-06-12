ae8ef7706343e40b4621caa58e592e95b23b1f89
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-12 21:42:44 +0300

    size_t N,A,B
    Solved N= 100000000  in  88605 ms(linear solver only)

--- Functions/globals.h
+++ Functions/globals.h
@@ -3,7 +3,7 @@ typedef int64_t i64;
 typedef uint32_t u32;
 
 int nosolve=0,fileload=0,scram=0,doprint=0,dofile=0;
-val_t N,A=0,B=1;
+size_t N,A=0,B=1;
 val_t * board;
 val_t * diagL;i64 sumL=0;
 val_t * diagR;i64 sumR=0;
