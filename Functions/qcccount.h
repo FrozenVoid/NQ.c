ce09a4360513c00ecf3f9fd2e26c9eb48cd8e60b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-13 18:18:33 +0300

    Update qcccount.h

--- Functions/qcccount.h
+++ Functions/qcccount.h
@@ -1,17 +1,17 @@
-static inline int zerocols2( val_t P){
-const val_t s=board[P];
+static inline int zerocols2( size_t P){
+const size_t s=board[P];
 return likely(((diagL[s+P])+diagR[s+(N-P)])==2);
 }
 //queen collisons at position: 2=none,2+=collision
-static inline val_t qccount(val_t P){
+static inline size_t qccount(size_t P){
 //cannot be zero due being set from q[]
 const size_t s=board[P];
 __builtin_prefetch(&diagL[s+P],0,0);
 __builtin_prefetch(&diagR[s+(N-P)],0,0);
 return (((diagL[s+P]))+((diagR[s+(N-P)])))-2;}
 
-static inline val_t qrcount(val_t P){
+static inline size_t qrcount(size_t P){
 //raw count of collisions
-val_t s=board[P];
+size_t s=board[P];
 return (((diagL[s+P]))+((diagR[s+(N-P)])));}
 
