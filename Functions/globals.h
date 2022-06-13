bb92de5729a9d0e094008421df5b784885be4260
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-13 18:08:57 +0300

    Update globals.h

--- Functions/globals.h
+++ Functions/globals.h
@@ -1,13 +1,10 @@
-typedef uint64_t u64;
-typedef int64_t i64;
-typedef uint32_t u32;
 
-int nosolve=0,fileload=0,scram=0,doprint=0,dofile=0;
+uint8_t nosolve=0,fileload=0,scram=0,doprint=0,dofile=0;
 size_t N,A=0,B=1;
-val_t * board;
-val_t * diagL;i64 sumL=0;
-val_t * diagR;i64 sumR=0;
-u64 tfail=0, swapt=0,tswaps=0,edge;
-i64   fail=0;
+boardtype * board;
+boardtype * diagL;size_t sumL=0;
+boardtype * diagR;size_t sumR=0;
+size_t tfail=0, swapt=0,tswaps=0,edge;
+size_t   fail=0;
 size_t cend,cur,best,limswap=0;
 size_t NL;
