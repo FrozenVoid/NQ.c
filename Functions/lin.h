0942cd2153c7afe82091b8d70dbabe71dc75dcb1
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-13 23:05:01 +0300

    Update lin.h

--- Functions/lin.h
+++ Functions/lin.h
@@ -1,42 +1,28 @@
-//----------------Stage 3--------
-void NOINLINE linearsolveend(){if(!cur)return;
-esecond:;B=(rndedgecell(A));
-emidloop:;
-verbinfo("EMidloop:");//midloop
-swapc(A,B);cur=countudiag();
-if(unlikely(cur<best))goto egoodswap;
-fail++;incfails();verbinfo("EFail:");//fail test
-if(unlikely(fail>=failmax)){fail=0;best=cur;goto eresetA;}
-goto swapfix;egoodswap:;
-incswap();fail=0;
-info("ESwap:");//fail==0 -> goodswap
-best=cur;//new record
-if(unlikely(cur==0)){return;}
-//next iteration:
-if(unlikely(zerocols2(A))){goto eresetA;;}
-
-goto esecond;
-eresetA:;A=fstgcols(A);goto emidloop;
-swapfix:;swapc(A,B); goto esecond;
-
-}
 
 //--------------------Stage2---------------
-void NOINLINE linearsolve(){if(!cur)return;
-goto resetA;midloop:;
-verbinfo("Midloop:");
-swapc(A,B);cur=countudiag();
-if(likely(cur>best))goto nfail;
-goodswap:;incswap();fail=0;
+void NOINLINE linearsolve(){
+const size_t stage3lim=119+N/512;
+const size_t failmax=NL*NL;
+void*nextB=&&resetBfst;
+void* skip=&&skip1;
+if(!cur)return;
+print("\nStage2: ",mstime(),"ms Stage3Lim:",stage3lim,"cols\n");
+goto resetA;midloop:;verbinfo("Midloop:");
+swapauto(A,B);//badswap rejected
 info("Swap:");best=cur;//new record
-if(unlikely(cur<Blim))return linearsolveend();
-if(zerocols2(A)){
+//exit check
+if(unlikely(!cur))return;
+goto *skip;
+skip1:;
+if(unlikely(cur<stage3lim)){nextB=&&resetBrnd;skip=&&skip2;}
+//A refresh
+skip2:;
+if(unlikely(zerocols2(A))||fail>failmax){verbinfo("ResetA");
+fail=0;
 resetA:;A=fstgcols(A);goto midloop;}
-goto resetB;//nextB.
-nfail:;fail++;incfails();
-verbinfo("Fail:");//fail test
-if(unlikely(fail>failmax2)){
-fail=0;goto resetA;}
-swapc(A,B);
-resetB:;B=fstgcols(B);goto midloop;
+
+goto *nextB;
+//next swap
+resetBrnd:B=rndedgecell(A);goto midloop;
+resetBfst:B=fstgcols(B);goto midloop;
 }
